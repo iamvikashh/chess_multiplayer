902cf44afc1bd043b2aea70e7ce8dbc8fa19a161
vikash <vs9617925319@gmail.com>
2023-06-06 18:55:48 +0530

    added the states for chessGame component

--- src/chess/ui/chessgame.jsx
+++ src/chess/ui/chessgame.jsx
@@ -4,12 +4,18 @@ import React from 'react'
 
 class ChessGame extends React.Component{
     state={
+        gameState:"",
+        ddraggedPieceTargetId: "", // empty string means no piece is being dragged
+        playerTurnToMoveIsWhite: true,
+        whiteKingInCheck: false, 
+        blackKingInCheck: false
 
     }
     
     render(){
         return <React.Fragment>
             <div style={{backgroundImage: `url(${Board})`,width:"720px",height:"720px"}}>
+                
             </div>
         </React.Fragment>
     }
