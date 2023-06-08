27a2f60d3473ea3bf0675c7853dfd167d625ef07
vikash <vs9617925319@gmail.com>
2023-06-08 20:45:10 +0530

    added the ui for chess board pieces

--- src/chess/ui/chessgame.jsx
+++ src/chess/ui/chessgame.jsx
@@ -1,8 +1,12 @@
 import Board from '../asset/chessBoard.png'
 import React from 'react'
 import Game from '../model/chess'
-import {Stage, Layer } from 'react-konva'
+import {Stage, Layer,Rect} from 'react-konva'
 import Square from '../model/square'
+import Piece from './piece'
+import piecemap from './piecemap'
+
+
 
 
 class ChessGame extends React.Component{
@@ -18,12 +22,31 @@ class ChessGame extends React.Component{
         return <React.Fragment>
             <div style={{backgroundImage: `url(${Board})`,width:"720px",height:"720px"}}>
                 <Stage width = {720} height = {720}>
-                    <Layer> 
-                        <React.Fragment>
-                      
-                        </React.Fragment>
-                        
-                        
+                    <Layer>
+                    {this.state.gameState.getBoard().map((row) => {
+                        return (<React.Fragment>
+                                {row.map((square) => {
+                                    if (square.isOccupied()) {                                    
+                                        return (
+                                            <Piece 
+                                                x = {square.getCanvasCoord()[0]}
+                                                y = {square.getCanvasCoord()[1]} 
+                                                imgurls = {piecemap[square.getPiece().name]}
+                                                isWhite = {square.getPiece().color === "white"}
+                                                draggedPieceTargetId = {this.state.draggedPieceTargetId}
+                                                onDragStart = {this.startDragging}
+                                                onDragEnd = {this.endDragging}
+                                                id = {square.getPieceIdOnThisSquare()}
+                                                thisPlayersColorIsWhite = {this.props.color}
+                                                playerTurnToMoveIsWhite = {this.state.playerTurnToMoveIsWhite}
+                                                whiteKingInCheck = {this.state.whiteKingInCheck}
+                                                blackKingInCheck = {this.state.blackKingInCheck}
+                                                />)
+                                    }
+                                    return
+                                })}
+                            </React.Fragment>)
+                    })}
                     </Layer>
                 </Stage>
             </div>
